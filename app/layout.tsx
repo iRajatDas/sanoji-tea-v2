2e80667a90e055d226efa8262755dc53013d83c3
Rajat Das <rajat@Rajats-MacBook-Air.local>
2023-10-27 17:19:52 +0530

    sticky nav

--- app/layout.tsx
+++ app/layout.tsx
@@ -9,6 +9,7 @@ import { SiteBlob } from "@/components/site-blob";
 import { SiteFooter } from "@/components/site-footer";
 import { SiteHeader } from "@/components/site-header";
 import { Toaster } from "sonner";
+import Link from "next/link";
 
 export const metadata: Metadata = {
   title: siteConfig.name,
@@ -40,6 +41,22 @@ export default function RootLayout({ children }: RootLayoutProps) {
               <SiteBlob />
               <div className="relative flex-1">{children}</div>
               <SiteFooter />
+              {/* <div className="sticky bottom-0 bg-[#9d795c] backdrop-blur-3xl w-full sm:hidden">
+                <div className="grid text-zinc-100 grid-cols-2 divide-x-2 divide-yellow-900 border-t-- border-yellow-900 font-semibold">
+                  <Link
+                    href="tel:+917982432980"
+                    className="px-4 py-3.5 text-center"
+                  >
+                    Call Now
+                  </Link>
+                  <Link
+                    href="email:feedback@sanojitea.com"
+                    className="px-4 py-3.5 text-center"
+                  >
+                    Email Us
+                  </Link>
+                </div>
+              </div> */}
             </div>
           </Providers>
         </body>
